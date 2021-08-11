5effb8f4aeb59a2259cd939983ea9b568b2e62b1
Pushpendra Sharma <pushpendra.sharma@accolite.com>
2021-08-11 20:55:00 +0530

    Dropdown and Delete Component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import DeleteQuestionButton from "./components/CreateFormComponents/DeleteQuestionButton/DeleteQuestionButton"
+
 function App() {
 	return <div className='App'>Survey Project</div>;
 }
---
+++ src/components/CreateFormComponents/DropdownMenu/DropdownMenu.js
@@ -0,0 +1,35 @@
+// import { DropdownButton} from 'react-bootstrap';
+import React,{useState} from 'react'
+import { Dropdown, DropdownButton,  } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Dropdown } from 'bootstrap';
+
+
+function DropdownMenu(props) {
+    // const [props, setProps] = useState({questionType: "SINGLE"});
+    const options = {
+        "SINGLE": "Single Choice",
+        "MULTIPLE": "Multiple Choice",
+        "DESCRIPTIVE": "Descriptive",
+        "STAR": "Star Based"
+    }
+    
+    const onSelect = (e)=>{
+        // console.log(e);
+        // setProps({questionType: e});
+         props.questionTypeChangeHandler(props.questionId, e);
+    }
+    return (
+        <div >
+            <DropdownButton onSelect={onSelect} variant="light" title={options[props.questionType]}>
+            {
+                Object.keys(options).map((key, id)=>(
+                    <Dropdown.Item key={id} eventKey={key}>{options[key]}</Dropdown.Item>
+                ))
+            }
+            </DropdownButton>
+        </div>
+    )
+}
+
+export default DropdownMenu
