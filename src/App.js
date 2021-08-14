353d45b8f7baf6f68645f7e23f769955de42bb4f
Abhishek Kumar Yadav <aky12@iitbbs.ac.in>
2021-08-14 17:24:18 +0530

    Added dropdown button and updated paging functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
-function App() {
-	return <div className='App'>Survey Project</div>;
-}
+import React, { useState } from 'react'
+import PopDown from './components/CreateFormComponents/PopDown.js';
+import Paging from './components/CreateFormComponents/Paging.js'
+export const App = () => {
+	const totalQuestions = 100;
+	const [currentPage, setCurrentPage] = useState(1);
+	const [questionsPerPage, setquestionsPerPage] = useState(10);
+	const pagechangerequesthandler = (number) => {
+		setCurrentPage(number);
+	}
+	const questionsPerPageHandler = (option) => {
+		setquestionsPerPage(option);
+	}
+	return (
+		<div>
+			<PopDown
+				totalQuestions={totalQuestions}
+				questionsPerPageHandler={questionsPerPageHandler}
+			></PopDown>
+			<h1>Hello</h1>
+			<p>dvfffffffffffffffffddddddddddddddddddddddddddddddddddddddddddddddd
+				fdddddddddddddddddddddddvdffffffffffffffffffffffffffffffffffffffff
+				vdffffffffffffffffffffffffffffffffffffffff
+				dfvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+				dfvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+				dfvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+			</p>
+			<Paging totalQuestions={totalQuestions}
+				questionsPerPage={questionsPerPage}
+				pageChangeRequestHandler={pagechangerequesthandler}
+				currentPage={currentPage} >
 
+			</Paging>
+		</div>
+	)
+}
 export default App;
