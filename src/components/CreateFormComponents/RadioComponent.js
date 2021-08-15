3ee5dcc94dc6c2e45328718554ea95b35358075f
Prasana Venkatesh <prasana.seshadri@gmail.com>
2021-08-15 16:33:21 +0530

    Changes for integration

--- src/components/CreateFormComponents/RadioComponent.js
+++ src/components/CreateFormComponents/RadioComponent.js
@@ -1,149 +1,123 @@
-import React, { useState } from "react";
-import { BsX } from "react-icons/bs";
+import React, { useState } from 'react';
+import { BsX } from 'react-icons/bs';
 
 function RadioComponent(props) {
+	const [isEdit, setIsEdit] = useState([false, false, false]);
 
-    const [options, setOptions] = useState([{ optionId: 0, option: "option1" }, { optionId: 1, option: "option2" }]);
-    const [isEdit, setIsEdit] = useState([false, false]);
-    const [initial, setInitial] = useState('');
+	const { options } = props;
 
+	//QuestionChange
+	const handleChange = (e) => {
+		const isValid = e.target.value === '' ? false : true;
+		props.questionTextChangeHandler(props.questionId, e.target.value, isValid);
+	};
 
+	// ADD option
+	const handleOptionAdd = () => {
+		let newOption = 'option';
+		if (options.length === 10) {
+			window.alert('Maximum  limit(10) reached for adding');
+			return;
+		}
+		// setOptions([...options, newOption]);
+		props.questionOptionAddHandler(props.questionId, newOption);
 
+		let newo = [...isEdit];
+		newo.push(false);
+		setIsEdit(newo);
+	};
 
-    //QuestionChange
-    const handleChange = (e) => {
+	//After entering new Option in TextBox
+	const updateOption = (index) => {
+		if (options[index].option === '') {
+			window.alert('Option cannot be empty');
+			return;
+		}
 
-        // props.isValid=true;
-        setInitial(e.target.value);
-        //props.questionTextChangeHandler(props.questionID,e.target.value,props.isValid);
-    }
-    // Question Empty Check
-    const checkValid = () => {
+		let newo = [...isEdit];
+		newo.splice(index, 1, false);
+		setIsEdit(newo);
+	};
 
-        if (initial === "") {
-            // props.isValid=false;
-            window.alert("NO empty allowed")
-            return
-        }
+	// Remove Option
+	const handleRemove = (index) => {
+		props.questionOptionRemoveHandler(props.questionId, props.options[index].optionId);
+	};
 
-    }
+	//OnCLickingLabel
+	const addOption = (index) => {
+		let newo = [...isEdit];
+		newo.splice(index, 1, true);
 
-    // ADD option
-    const handleOptionAdd = () => {
+		setIsEdit(newo);
+	};
 
-        let newOption = { optionId: options.length, option: "option" }
-        if (options.length === 10) {
-            window.alert("Maximum  limit(10) reached for adding")
-            return
-        }
-        setOptions([
-            ...options,
-            newOption,
-        ]);
+	// Updated Text from TextBox
+	const updateValue = (e, i) => {
+		// let op = options[index];
+		// op.option = e.target.value;
+		// let newo = [...options];
+		// newo.splice(index, 1, op);
 
-        let newo = [...isEdit]
-        newo.push(false)
-        setIsEdit(newo);
+		// setOptions(newo);
+		props.questionOptionChangeHandler(
+			props.questionId,
+			props.options[i].optionId,
+			e.target.value
+		);
+	};
 
-        //questionOptionAddHandler(props.questionId, newOption)
-    };
+	return (
+		<div className='App'>
+			<div className='mt-5'>
+				<input
+					className='form-control'
+					value={props.question}
+					type='text'
+					placeholder='Question'
+					size='50'
+					maxLength='100'
+					onChange={handleChange}
+				/>
+				<br />
+				<br />
+			</div>
+			<div>
+				{options.map((choice, i) => {
+					return (
+						<div key={i}>
+							<input type='radio' value={choice.value} />
+							{isEdit[i] === false ? (
+								<label onClick={() => addOption(i)}>{choice.option}</label>
+							) : (
+								<input
+									type='text'
+									value={options[i].option}
+									maxLength='100'
+									onBlur={() => updateOption(i)}
+									onChange={(e) => updateValue(e, i)}
+								/>
+							)}
 
-
-    //After entering new Option in TextBox
-    const updateOption = (index) => {
-        if (options[index].option === "") {
-            window.alert("Option cannot be empty")
-            return
-        }
-
-         let newOption = options[index]
-
-
-        let newo = [...isEdit]
-        newo.splice(index, 1, false)
-        setIsEdit(newo)
-
-        // props.questionOptionChangeHandler(props.questionId,props.OptionId,newOption)
-    }
-
-    // Remove Option
-    const handleRemove = index => {
-        const list = [...options];
-        list.splice(index, 1)
-        let OptionId=index
-        setOptions(list)
-        // props.questionRemoveHandler(props.questionId,OptionId)
-    }
-
-
-    //OnCLickingLabel
-    const addOption = index => {
-
-
-        let newo = [...isEdit]
-        newo.splice(index, 1, true)
-
-        setIsEdit(newo)
-
-
-    }
-
-    //Updated Text from TextBox
-    const updateValue = (e, index) => {
-
-
-        let op = options[index]
-        op.option = e.target.value
-        let newo = [...options]
-        newo.splice(index, 1, op)
-
-        setOptions(newo)
-
-    }
-
-
-    return (
-
-        <div className="App">
-
-            <div className="mt-5">
-                <input className="form-control" value={initial} type="text" maxLength="100" placeholder="Question" size="50" onChange={handleChange} onBlur={checkValid}/>
-                <br />
-                <br />
-
-            </div>
-            <div>
-                {options.map((choice, i) => {
-                    return (
-                        <div key={i}>
-                            <input
-                                type="radio"
-                                name="dynamic-radio"
-                                value={choice.value}
-                            //checked={radioValue === option.value}
-                            //onChange={(e) => setRadioValue(e.target.value)}
-
-                            />
-                            {
-                                (isEdit[i] === false) ? <label onClick={() => addOption(i)}>{choice.option}</label> :
-
-                                    <input type="text" value={options[i].option} maxLength="100" onBlur={() => updateOption(i)} onChange={(e) => updateValue(e, i)} />
-                            }
-
-                            {
-                                (options.length > 2) ?
-                                    <button onClick={() => handleRemove(i)}><BsX /></button> : <p></p>
-                            }
-                        </div>
-                    )
-                })}
-            </div>
-            <br />
-            <div>
-                <button className="btn btn-primary" onClick={() => handleOptionAdd()}>Add Option</button>
-            </div>
-        </div>
-    );
+							{options.length > 3 ? (
+								<button onClick={() => handleRemove(i)}>
+									<BsX />
+								</button>
+							) : (
+								<p></p>
+							)}
+						</div>
+					);
+				})}
+			</div>
+			<br />
+			<div>
+				<button className='btn btn-primary' onClick={handleOptionAdd}>
+					Add Option
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default RadioComponent;
