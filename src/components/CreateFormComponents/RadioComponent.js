608d071c9e27d3a62e1865b22576243c064b194b
Prasana Venkatesh <51082101+PrasanaVenkateshS@users.noreply.github.com>
2021-08-16 16:57:40 +0530

    Update RadioComponent.js

--- src/components/CreateFormComponents/RadioComponent.js
+++ src/components/CreateFormComponents/RadioComponent.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BsX } from 'react-icons/bs';
+import { BsFillTrashFill } from 'react-icons/bs';
 
 function RadioComponent(props) {
 	const [isEdit, setIsEdit] = useState([false, false, false]);
@@ -74,9 +74,9 @@ function RadioComponent(props) {
 					className='form-control'
 					value={props.question}
 					type='text'
-					placeholder='Question'
+					placeholder='Question - Maximum 250 Characters'
 					size='50'
-					maxLength='100'
+					maxLength='250'
 					onChange={handleChange}
 				/>
 				<br />
@@ -85,25 +85,26 @@ function RadioComponent(props) {
 			<div>
 				{options.map((choice, i) => {
 					return (
-						<div key={i}>
-							<input type='radio' value={choice.value} />
-							{isEdit[i] === false ? (
-								<label onClick={() => addOption(i)}>{choice.option}</label>
-							) : (
-								<input
-									type='text'
-									autoFocus="autofocus"
-									value={options[i].option}
-									maxLength='100'
-									onBlur={() => updateOption(i)}
-									onChange={(e) => updateValue(e, i)}
-								/>
-							)}
-
+						<div key={i} className="row">
+							<div className="col-md-8">
+								<input type='radio' value={choice.value} />
+								{isEdit[i] === false ? (
+									<label onClick={() => addOption(i)}>{choice.option}</label>
+								) : (
+									<input
+										type='text'
+										value={options[i].option}
+										maxLength='250'
+										autoFocus="autofocus"
+										onBlur={() => updateOption(i)}
+										onChange={(e) => updateValue(e, i)}
+									/>
+								)}
+							</div>
 							{options.length > 2 ? (
-								<button onClick={() => handleRemove(i)}>
-									<BsX />
-								</button>
+								<div className="col-md-4"><button className="float-end" onClick={() => handleRemove(i)}>
+									<BsFillTrashFill />
+								</button></div>
 							) : (
 								<p></p>
 							)}
@@ -117,6 +118,7 @@ function RadioComponent(props) {
 					Add Option
 				</button>
 			</div>
+			<br></br>
 		</div>
 	);
 }
