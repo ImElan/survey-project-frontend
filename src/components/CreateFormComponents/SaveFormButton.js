d99195532ab41e04adaaeb8a600f0c270f101f5c
Syed Ashik Siddiq M <62946935+ashik85@users.noreply.github.com>
2021-08-16 11:27:20 +0530

    Update SaveFormButton.js

--- src/components/CreateFormComponents/SaveFormButton.js
+++ src/components/CreateFormComponents/SaveFormButton.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
-import {Button} from 'react-bootstrap';
+import {Button, Alert} from 'react-bootstrap';
 import PopUpModal from './PopUpModal';
 
 
 function SaveFormButton(props) {
         
-        const {formTitle, saveFormHandler} = props;
+        const {formTitle, questionList, saveFormHandler} = props;
 
         const [show, popup] = useState(false);
+        const [err, setAlert] = useState(false);
         const [popUpTitle] = useState("");
         const [popUpBody] = useState(`Are you sure you want to save "${formTitle}" form?`);
 
@@ -17,14 +18,46 @@ function SaveFormButton(props) {
         const popUpClose = () => {
             popup(false);
         }
-    
+
+        const showError = () => {
+            setAlert(true);            
+        }
+
+        const errorCheck = () => {
+            
+            let flag = true;   
+            questionList.forEach(function(question){
+                if(question.isValid == false){
+                    flag = false;
+                }
+            });
+
+            if(flag){
+                //no error
+                popUpOpen();
+            }
+
+            else{
+                //error found
+                showError();
+            }
+        }
+
+
       return (     
             <div className= "text-center">
-                <Button variant = "primary"
-                        className = "bg-light-blue b--blue black f5 fw7 bw1 grow pointer"
-                        onClick = {popUpOpen}> Save Form
+                <Button variant = 'dark'
+                        className = "bg-purple f4 fw5 bw1 grow pointer"
+                        onClick = {errorCheck}> Save Form
                 </Button>
-                <PopUpModal show = {show} popUpClose = {popUpClose} popUpTitle = {popUpTitle} popUpBody = {popUpBody} confirmHandler = {saveFormHandler}/>
+
+                <Alert show={err} variant="danger">
+                    <h5>Could not save! Some empty fields found in your form.</h5>
+                    <Button onClick={() => setAlert(false)} variant="outline-danger">Close!
+                    </Button>
+                </Alert>
+
+                <PopUpModal show ={show} popUpClose ={popUpClose} popUpTitle={popUpTitle} popUpBody={popUpBody} confirmHandler={saveFormHandler}/>
             </div>                 
         );
 }
