25fd109f9d349cb61650eeffeaf4ac8e2615af02
Vijayakumar512 <88498574+Vijayakumar512@users.noreply.github.com>
2021-08-16 17:29:45 +0530

    Updated for demo

--- src/components/CreateFormComponents/NameForm.js
+++ src/components/CreateFormComponents/NameForm.js
@@ -1,41 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component,useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Form, InputGroup, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
 function NameForm (props){
-
+  const [show1, formtit] = useState(false);
+  const [show2, formdesc] = useState(false);
+  const showtext1=()=>{
+    formtit(true);
+  };
+  const showtext2=()=>{
+    formdesc(true);
+  };
   const handleChange1=(event)=> {
-    props.titleChangeHandler(event.target.value)
-  }
+    var x=event.target.value;
+    if(x.length>((event.maxLength||0)+5))
+    showtext1();
+    else
+    props.titleChangeHandler(event.target.vaue);
+  };
 
-  const handleChange2=(event)=> {
+    const handleChange2=(event)=> {
+      var x=event.target.value;
+      if(x.length>((event.maxLength||0)+5))
+      showtext2();
+      else
      props.descriptionChangeHandler(event.target.value);
-    }
+   };
 
-   return (
-       <div>
-          <FormControl 
-              aria-label="Default" 
-              aria-describedby="inputGroup-sizing-default" 
-              placeholder="Enter form name" 
-              maxLength="40" 
-              value={props.title} 
-              onChange={handleChange1}
-          />
-          <Form.Text id='passwordHelpBlock' muted style={{ display: 'block' }}> 
-              Form title cannot exceed 40 characters
-          </Form.Text>
-          <FormControl 
-              as="textarea" 
-              aria-label="With textarea" 
-              placeholder="Enter form description" 
-              maxLength="100" 
-              value={props.description} 
-              onChange={handleChange2} 
-          />
-          <Form.Text id='passwordHelpBlock' muted style={{ display: 'block' }}> 
-              Form description cannot exceed 100 characters
-          </Form.Text>
+    return (
+      <div>
+      <br/>
+    <InputGroup className="mb-3">
+    <Form.Label style={{border: "1px solid #FF00FF", width: "150px", background: "#FFE8EF", padding: "3px", display: "flex"}}>Form Name</Form.Label>
+    <FormControl style={{display: "flex"}}aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Enter form name" maxLength="80"  value={props.description} onChange={handleChange1}/>
+      {show1 && <Form.Text id="passwordHelpBlock" muted>Form title cannot exceed 5 characters</Form.Text>}
+    </InputGroup>
+    <InputGroup classname="mb-3">
+    <Form.Label style={{border: "1px solid #FF00FF", width: "150px", background: "#FFE8EF", padding: "3px"}}>Form Description</Form.Label>
+    <FormControl  as="textarea" aria-label="With textarea" placeholder="Enter form description" maxLength="250" value={props.description} onChange={handleChange2} />
+     {show2 && <Form.Text id="passwordHelpBlock" muted>Form description cannot exceed 5 characters</Form.Text>}
+  </InputGroup>
       </div>
     );
   }
