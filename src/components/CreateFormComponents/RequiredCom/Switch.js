b88b7d322f0113e54552f48e99b514eb58228875
Elan <elansakthivel2000@gmail.com>
2021-08-16 17:29:11 +0530

    integrate code-review changed required component

--- src/components/CreateFormComponents/RequiredCom/Switch.js
+++ src/components/CreateFormComponents/RequiredCom/Switch.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import "./Switch.css";
-import cx from "classname";
-
+import './Switch.css';
+import cx from 'classname';
 
 const Switch = (props) => {
-    const sliderCX = cx("slider", {
-        'rounded': props.rounded
-    });
-    const onToggle = () => props.requiredChangeHandler(props.questionId);
-    return (
-        <label className= "switch">
-            <input type="checkbox" checked={props.required} onChange={onToggle} />
-            <span className={sliderCX}/>
-        </label>
-    );
+	const sliderCX = cx('slider', {
+		rounded: props.rounded,
+	});
+	const onToggle = () => props.requiredChangeHandler(props.questionId);
+	return (
+		<div
+			style={{
+				display: 'flex',
+				padding: '5px 0',
+				alignItems: 'center',
+			}}
+		>
+			<label className='switch'>
+				<input type='checkbox' checked={props.required} onChange={onToggle} />
+				<span className={sliderCX} />
+			</label>
+			<h5 style={{ marginLeft: '8px' }}>Required</h5>
+		</div>
+	);
 };
 
 export default Switch;
