d7523aad8abb0286050e4f3fae6680ddc1c811dc
Gokkul A <30643994+Gokkul23@users.noreply.github.com>
2021-08-16 19:48:43 +0530

    Bugs Update Starcomponent.js

--- src/components/CreateFormComponents/StarComponent.js
+++ src/components/CreateFormComponents/StarComponent.js
@@ -2,7 +2,13 @@ import { useState } from 'react';
 import React from 'react';
 import { FaStar } from 'react-icons/fa';
 // import { makeStyles } from '@material-ui/core/styles';
-import { Form, ToggleButton, ButtonGroup, Dropdown, DropdownButton } from 'react-bootstrap';
+import {
+	Form,
+	ToggleButton,
+	ButtonGroup,
+	Dropdown,
+	DropdownButton,
+} from 'react-bootstrap';
 // import { TextField } from "@material-ui/core";
 
 // const colors = {
@@ -13,12 +19,6 @@ import { Form, ToggleButton, ButtonGroup, Dropdown, DropdownButton } from 'react
 function StarComponent(props) {
 	// const [currentValue, setCurrentValue] = useState(0);
 	// const [hoverValue, setHoverValue] = useState(undefined);
-	const [radioValue, setRadioValue] = useState('fullStarPointType');
-
-
-	const radios = [
-		{ name: 'Full Star Points', value: 'fullStarPointType' },
-		{ name: 'Half Star Points', value: 'halfStarPointType' }];
 
 	// const stars = Array(5).fill(0);
 
@@ -38,28 +38,33 @@ function StarComponent(props) {
 	// 	props.onChange(e.target.value);
 	// };
 
+	const [show1, formtitl] = useState(false);
+
+	const showtext2 = () => {
+		formtitl(true);
+	};
 	const [initial, final] = useState('');
 
 	const questionChange = (e) => {
+		var x = e.target.value;
+		if (x.length > (e.maxLength || 0) + 249) showtext2();
+		else{
 		final(e.target.value);
 		const isValid = e.target.value === '' ? false : true;
-		props.questionTextChangeHandler(props.questionId, e.target.value,isValid);
+		props.questionTextChangeHandler(props.questionId, e.target.value, isValid);
+		}
 	};
 
 	const typeStarChange = (e) => {
-		setRadioValue(e.currentTarget.value);
-		const isValid = e.target.value === '' ? false : true;
-		props.starTypechangeHandler(props.questionId, e.currentTarget.value,isValid);
+		props.starTypeChangeHandler(props.questionId);
 	};
 
-	const [value, setValue] = useState('');
+	const [value, setValue] = useState('3');
 
 	const numStarsChange = (e) => {
-		setValue(e)
-		const isValid = e.target.value === '' ? false : true;
-		props.starTypechangeHandler(props.questionId,e,isValid);
-
-	}
+		setValue(e);
+		props.starNumChangeHandler(props.questionId, e);
+	};
 
 	const options = {
 		3: '3',
@@ -69,16 +74,15 @@ function StarComponent(props) {
 		7: '7',
 		8: '8',
 		9: '9',
-		10: '10'
+		10: '10',
 	};
 
 	// const printQuestion = () => {
 	// 	return `The Content is ${value}`
 	// }
 
-
 	return (
-		<div >
+		<div>
 			<form>
 				{/* <TextField id="outlined-basic" label="Question" multiline={false}  onChange={handleChange} InputProps={{style:{width:'43ch'}}} /><br></br><br></br> */}
 				<Form>
@@ -86,21 +90,26 @@ function StarComponent(props) {
 						<Form.Label></Form.Label>
 						<Form.Control
 							style={{ height: 35 }}
-							size='lg'
-							type='text'
+							as='textarea'
 							onChange={questionChange}
 							title={props.question}
 							value={initial}
 							placeholder='Question - Maximum 250 Characters'
-							maxlength="250"
+							maxlength='250'
 						/>
-						<br></br>
-						<div >Number of stars required : </div>
+						{show1 && (
+							<Form.Text style={{ color: 'white' }} id='passwordHelpBlock' muted><br></br>
+									Question cannot exceed 250 characters
+							</Form.Text>
+						)}
+						<br></br><br></br>
+						<div>Number of stars required : </div><br></br>
 						<DropdownButton
 							alignRight
 							title={options[value]}
-							id="dropdown-menu-align-right"
+							id='dropdown-menu-align-right'
 							onSelect={numStarsChange}
+							value={props.numStars}
 						>
 							{Object.keys(options).map((key, id) => (
 								<Dropdown.Item key={id} eventKey={key}>
@@ -108,25 +117,29 @@ function StarComponent(props) {
 								</Dropdown.Item>
 							))}
 						</DropdownButton>
-						<br></br>
+						<br></br><br></br>
 
 						{/* <div>{printQuestion(value)}</div> */}
 
 						<ButtonGroup>
-							{radios.map((radio, idx) => (
-								<ToggleButton
-									key={idx}
-									id={`radio-${idx}`}
-									type="radio"
-									variant={idx % 2 ? 'outline-success' : 'outline-danger'}
-									name="radio"
-									value={radio.value}
-									checked={radioValue === radio.value}
-									onChange={typeStarChange}
-								>
-									{radio.name}
-								</ToggleButton>
-							))}
+							<ToggleButton
+								type='radio'
+								variant={'outline-primary'}
+								name='radio'
+								checked={!props.isHalfStarAllowed}
+								onClick={typeStarChange}
+							>
+								Full Star
+							</ToggleButton>
+							<ToggleButton
+								type='radio'
+								variant={'outline-primary'}
+								name='radio'
+								checked={props.isHalfStarAllowed}
+								onClick={typeStarChange}
+							>
+								Half Star
+							</ToggleButton>
 						</ButtonGroup>
 
 						{/* <div>{printQuestion(initial)}</div> */}
