a4c52cf11f25d5df32cc1c3703eb54c9f0c1d3ab
Syed Ashik Siddiq M <62946935+ashik85@users.noreply.github.com>
2021-08-18 01:04:47 +0530

    sprint 1 bugs fixed

--- src/components/CreateFormComponents/SaveFormButton.js
+++ src/components/CreateFormComponents/SaveFormButton.js
@@ -1,64 +1,87 @@
-import React, {useState} from 'react';
-import {Button, Alert} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Alert } from 'react-bootstrap';
 import PopUpModal from './PopUpModal';
 
-
 function SaveFormButton(props) {
+	const { formTitle, questionList, saveFormHandler } = props;
+
+	const [show, popup] = useState(false);
+	const [err, setAlert] = useState(false);
+	const [alertBody, setAlertBody] = useState('');
+	const [popUpTitle] = useState('Confirm Submission');
+	const [popUpBody] = useState(`Are you sure you want to save "${formTitle}" form?`);
+
+	const popUpOpen = () => {
+		popup(true);
+	};
+	const popUpClose = () => {
+		popup(false);
+	};
+
+	const showError = (msg) => {
+		setAlertBody(msg);
+		setAlert(true);
+	};
+
+	const errorCheck = () => {
+		let flag1 = true;
+		let questionArr = [];
+
+		questionList.forEach(function (question) {
+			if (question.isValid === false) {
+				flag1 = false;
+			}
+			questionArr.push(question.question);
+		    
+		});
         
-        const {formTitle, questionList, saveFormHandler} = props;
+		const questionSet = new Set(questionArr);
+                     const duplicateArr = questionArr.filter(item => {
+			if(questionSet.has(item))
+			    questionSet.delete(item);
+			else
+			    return item;
+		});
 
-        const [show, popup] = useState(false);
-        const [err, setAlert] = useState(false);
-        const [popUpTitle] = useState("Confirm Submission");
-        const [popUpBody] = useState(`Are you sure you want to save "${formTitle}" form?`);
+		
 
-        const popUpOpen = () => {
-            popup(true);
-        }
-        const popUpClose = () => {
-            popup(false);
-        }
+	    if (!flag1) {
+			showError('Could not save form! Empty fields found.');
+		}	
+	    else if(duplicateArr.length){
+			showError('Could not save form! Duplicate questions found.');
+		}
+	    else{
+			//no error
+			popUpOpen();
+		}
+	}
 
-        const showError = () => {
-            setAlert(true);            
-        }
+	return (
+		<div className='text-center'>
+			<Alert show={err} variant='danger'>
+				<h5>{alertBody}</h5>
+				<Button onClick={() => setAlert(false)} variant='outline-danger'>
+					Close!
+				</Button>
+			</Alert>
 
-        const errorCheck = () => {
-            
-            let flag = true;   
-            questionList.forEach(function(question){
-                if(question.isValid == false){
-                    flag = false;
-                }
-            });
+			<Button
+				variant='dark'
+				className='bg-purple f3 fw5 bw1 grow pointer'
+				onClick={errorCheck}
+			>
+				Save Form
+			</Button>
 
-            if(flag){
-                //no error
-                popUpOpen();
-            }
-
-            else{
-                //error found
-                showError();
-            }
-        }
-
-
-      return (     
-            <div className= "text-center">
-                <Alert show={err} variant="danger">
-                    <h5>Could not save! Some empty fields found in your form.</h5>
-                    <Button onClick={() => setAlert(false)} variant="outline-danger">Close!
-                    </Button>
-                </Alert>
-
-                <Button variant = 'dark'
-                        className = "bg-purple f4 fw5 bw1 grow pointer"
-                        onClick = {errorCheck}> Save Form
-                </Button>
-
-                <PopUpModal show ={show} popUpClose ={popUpClose} popUpTitle={popUpTitle} popUpBody={popUpBody} confirmHandler={saveFormHandler}/>
-            </div>                 
-        );
+			<PopUpModal
+				show={show}
+				popUpClose={popUpClose}
+				popUpTitle={popUpTitle}
+				popUpBody={popUpBody}
+				confirmHandler={saveFormHandler}
+			/>
+		</div>
+	);
 }
 export default SaveFormButton;
