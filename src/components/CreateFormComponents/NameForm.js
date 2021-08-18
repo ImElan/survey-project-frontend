63ef81f7ce4b8c8ffd83c59742fd819f207b53a5
Vijayakumar512 <88498574+Vijayakumar512@users.noreply.github.com>
2021-08-18 10:00:33 +0530

    bug fixed

--- src/components/CreateFormComponents/NameForm.js
+++ src/components/CreateFormComponents/NameForm.js
@@ -1,46 +1,78 @@
-import React, { Component,useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Form, InputGroup, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
-function NameForm (props){
-  const [show1, formtit] = useState(false);
-  const [show2, formdesc] = useState(false);
-  const showtext1=()=>{
-    formtit(true);
-  };
-  const showtext2=()=>{
-    formdesc(true);
-  };
-  const handleChange1=(event)=> {
-    var x=event.target.value;
-    if(x.length>((event.maxLength||0)+5))
-    showtext1();
-    else
-    props.titleChangeHandler(event.target.vaue);
-  };
+function NameForm(props) {
+	const [show1, formtit] = useState(false);
+	const [show2, formdesc] = useState(false);
+	const showtext1 = () => {
+		formtit(true);
+	};
+	const showtext2 = () => {
+		formdesc(true);
+	};
+	const showtext3 = () =>{
+		formtit(false);
+	};
+	const showtext4 = () =>{
+		formdesc(false);
+	};
+	const handleChange1 = (event) => {
+		var x = event.target.value;
+		if (x.length > (event.maxLength || 0) + 79) showtext1();
+		else 
+		{
+			showtext3();
+			props.titleChangeHandler(event.target.value);
+		}
+	};
 
-    const handleChange2=(event)=> {
-      var x=event.target.value;
-      if(x.length>((event.maxLength||0)+5))
-      showtext2();
-      else
-     props.descriptionChangeHandler(event.target.value);
-   };
+	const handleChange2 = (event) => {
+		var x = event.target.value;
+		if (x.length > (event.maxLength || 0) + 249) showtext2();
+		else {
+			showtext4();
+			props.descriptionChangeHandler(event.target.value);
+		}
+	};
 
-    return (
-      <div>
-      <br/>
-    <InputGroup className="mb-3">
-    <Form.Label style={{border: "1px solid #FF00FF", width: "150px", background: "#FFE8EF", padding: "3px", display: "flex"}}>Form Name</Form.Label>
-    <FormControl style={{display: "flex"}}aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Enter form name" maxLength="80"  value={props.description} onChange={handleChange1}/>
-      {show1 && <Form.Text id="passwordHelpBlock" muted>Form title cannot exceed 5 characters</Form.Text>}
-    </InputGroup>
-    <InputGroup classname="mb-3">
-    <Form.Label style={{border: "1px solid #FF00FF", width: "150px", background: "#FFE8EF", padding: "3px"}}>Form Description</Form.Label>
-    <FormControl  as="textarea" aria-label="With textarea" placeholder="Enter form description" maxLength="250" value={props.description} onChange={handleChange2} />
-     {show2 && <Form.Text id="passwordHelpBlock" muted>Form description cannot exceed 5 characters</Form.Text>}
-  </InputGroup>
-      </div>
-    );
-  }
+	return (
+		<div>
+			<br />
+			<Form.Label style={{ color: 'white' }}>Form Name</Form.Label>
+			<FormControl
+				aria-label='Default'
+				aria-describedby='inputGroup-sizing-default'
+				placeholder='Enter form name'
+				maxLength='80'
+				value={props.title}
+				onChange={handleChange1}
+			/>
+			{show1 && (
+				<div>
+					<Form.Text style={{ color: 'red' }} id='passwordHelpBlock' muted>
+						Form title cannot exceed 80 characters
+					</Form.Text>
+					<br />
+				</div>
+			)}
+			<Form.Label style={{ marginTop: '10px', color: 'white' }}>
+				Form Description
+			</Form.Label>
+			<FormControl
+				as='textarea'
+				aria-label='With textarea'
+				placeholder='Enter form description'
+				maxLength='250'
+				value={props.description}
+				onChange={handleChange2}
+			/>
+			{show2 && (
+				<Form.Text style={{ color: 'red' }} id='passwordHelpBlock' muted>
+					Form description cannot exceed 250 characters
+				</Form.Text>
+			)}
+		</div>
+	);
+}
 export default NameForm;
