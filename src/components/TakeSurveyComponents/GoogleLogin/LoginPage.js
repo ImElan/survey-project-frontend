8e9b1094a963b6844edf0676798510c5e1be3b9f
Lokesh Singh <lokesh.singh@accolitedigital.com>
2021-08-21 21:21:03 +0530

    included differentiation between hr/employee

--- src/components/TakeSurveyComponents/GoogleLogin/LoginPage.js
+++ src/components/TakeSurveyComponents/GoogleLogin/LoginPage.js
@@ -5,50 +5,38 @@ import axios from 'axios';
 
 function LoginPage() {
 
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const handleChange1 = (e) => {
-    setUsername(e.target.value)
-  }
- 
-  const handleChange2 = (e) => {
-    setPassword(e.target.value)
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(username + " " + password)
-  }
+  const [isEmployee,setEmployee] = useState(false);
+  const [isHr,setHr] = useState(false);
+  const [isValid,setValid] = useState(false);
+  const [inValid,setInValid] = useState(false);
 
   const responseSuccessGoogle = (response) => {
 		console.log(response)
-    // axios({
-    //   method: "POST",
-    //   url : "http://localhost:3000/api/auth/login/oauth/google",
-    //   data : {tokenId : response.tokenId}
-    // }).then(response => {
-    //   console.log(response)
-    // })
+    axios.get('http://localhost:3000/api/auth/login/oauth/google', {
+      headers : {
+        Authorization : 'Bearer ${idToken}'
+      }
+    }).then(response => {
+      console.log(response)
+      if(response.user.role.equals("EMPLOYEE")){
+        setEmployee(true);
+      }else{
+        setHr(true);
+      }
+    })
+    setValid(true);
+    window.localStorage.setItem("googleResponse", JSON.stringify(response));
 	}
   
 	const responseErrorGoogle = (response) => {
 	  console.log(response)
+     setInValid(true)
 	}
 
               return(
                   <div className='div-login'>
                       <div className = 'Title'>
                           <h2>Survey Form</h2>
-                          {/* <div>
-                            <form onSubmit={handleSubmit}>
-                                  <input type='email' name='email' placeholder='email...' required onChange={handleChange1}/>
-                            
-                                  <input type='password' name='pwd' placeholder='password...' required onChange={handleChange2}/>
-
-                                  <button type = 'submit' >Sign in</button>
-                            </form>
-                          </div> */}
                           <div className = "newuser">
                           <GoogleLogin className = "google"
                               clientId="104208248429-bt7t5eo6pce3db752p4rbdh9ica46ap1.apps.googleusercontent.com"
@@ -59,10 +47,21 @@ function LoginPage() {
                           />
                           </div>
                       </div>
+                      
                       <div className = "company">
                             <h3 className= "accolite">Accolite</h3>
                             <h3 className= "digital">Digital</h3>
-                          </div>
+                      </div>
+                      <div className= "message">
+                      {isValid ?
+                        <h4>LogIn Successful</h4>
+                        : null
+                      }
+                      {
+                        inValid ? <h4 className = "tryagain">Please try again!</h4> 
+                        : null
+                      }
+                      </div>
                   </div>
               )      
   }
