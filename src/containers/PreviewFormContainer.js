34dc34392d8059af46e07f55a13528dc6125373a
Vijayakumar512 <88498574+Vijayakumar512@users.noreply.github.com>
2021-08-22 19:26:23 +0530

    Add files via upload

---
+++ src/containers/PreviewFormContainer.js
@@ -0,0 +1,83 @@
+import FormHeader from '../components/CreateFormComponents/NameForm';
+import { Container, Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+function PreviewFormContainer(props)
+{
+  const [newstate, setnewstate] = useState();
+  useEffect(()=>{
+    const formstate =JSON.parse(window.localStorage.getItem('formstate'));
+    //console.log(formstate);
+    setnewstate(formstate);
+    //console.log('preview form loaded');
+    //console.log(newstate);
+  },[]);
+  return(
+    <h1>TakeSurveyContainer state={newstate}</h1>
+  );
+  // return (
+	// 	<Container fluid>
+	// 		<Row
+	// 			className='justify-content-md-center'
+	// 			style={{
+	// 				backgroundColor: '#4B0082', //4B0082
+	// 				paddingTop: '0px',
+	// 				paddingBottom: '35px',
+	// 			}}
+	// 		>
+  //     <Col sm={6}>
+  //       <input type="text" value="formname"/>
+  //       <input type="text" value="formdescription"/>
+  //     </Col>
+  //   </Row>
+  //   <Row className='justify-content-md-center'>
+  //     <div
+  //       style={{
+  //         backgroundColor: '#D8D8D8',
+  //         width: '75%',
+  //         border: 'solid lightgray 2px',
+  //         borderTop: '0px',
+  //         borderRadius: '8px',
+  //       }}
+  //     >
+  //       <Row
+  //         sm='auto'
+  //         className='justify-content-end'
+  //         style={{
+  //           padding: '12px',
+  //         }}
+  //       >
+  //       </Row>
+  //
+  //
+  //       <Row
+  //         sm='auto'
+  //         className='justify-content-end'
+  //         style={{
+  //           padding: '12px',
+  //         }}
+  //       >
+  //       <Col sm={6}>
+  //         <input type="text" value="formname"/>
+  //         <input type="text" value="formdescription"/>
+  //       </Col>
+  //       </Row>
+  //       <Row
+  //         sm='auto'
+  //         className='justify-content-end'
+  //         style={{
+  //           padding: '12px',
+  //         }}
+  //       >
+  //       <Col sm={6}>
+  //         <input type="text" value="formname"/>
+  //         <input type="text" value="formdescription"/>
+  //       </Col>
+  //       </Row>
+  //
+  //   </div>
+  //   </Row>
+  //
+  //     </Container>
+  //   );
+}
+export default PreviewFormContainer;
