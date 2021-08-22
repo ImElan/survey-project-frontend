61eff28f32843fe1825b5b35db2d242f1c03eae7
Vijayakumar512 <88498574+Vijayakumar512@users.noreply.github.com>
2021-08-22 19:20:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import CreateFormContainer from './containers/CreateFormContainer';
+import PreviewFormContainer from './containers/PreviewFormContainer';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route
+} from 'react-router-dom'
+
 function App() {
-	return <div className='App'>Survey Project</div>;
+	return (
+		<div>
+		<Router>
+				<Route exact path = '/' component = {CreateFormContainer} />
+				<Route exact path = '/preview' component = {PreviewFormContainer} />
+			</Router>
+
+		</div>
+	);
 }
 
 export default App;
