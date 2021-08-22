5913ceef5f8ab2cbbbce722841c3567850f78347
Lokesh Singh <lokesh.singh@accolitedigital.com>
2021-08-23 02:00:23 +0530

    Fixex the page reloaded issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import LoginPage from './components/TakeSurveyComponents/GoogleLogin/LoginPage';
-// import { LoginProvider } from './components/TakeSurveyComponents/GoogleLogin/LoginContext';
 
 function App() {
 	return (
-		// <LoginProvider>
 			<div>
 				<LoginPage />
 			</div>
-		// </LoginProvider>
 	);  
 }
 export default App;
