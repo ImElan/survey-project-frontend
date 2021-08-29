80a322becaa78b2f82cc761c29e829a7cba9a9ee
Abhishek Kumar Yadav <aky12@iitbbs.ac.in>
2021-08-29 17:32:29 +0530

    ashik integration done

--- src/components/ResponseSummary/responseSummary.js
+++ src/components/ResponseSummary/responseSummary.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 // import from 'react';
 import CenteredTabs from './centeredTabs';
 import responseservice from './services/responseservice';
-import { ExportResponse } from '../ViewResponseComponents/ExportResponsesByFormId/ExportResponse';
+import ExportResponse from '../ViewResponseComponents/ExportResponsesByFormId/ExportResponse';
 import { Row, Col, Container } from 'react-bootstrap';
 const ResponseSummary = ({ formId }) => {
     const [display, setdisplay] = useState("");
@@ -24,20 +24,20 @@ const ResponseSummary = ({ formId }) => {
     //         })
     // })
     return (
-        <div>
-            {console.log("OK")}
-            <CenteredTabs change={change}></CenteredTabs>
-            {/* <ExportResponse formId={formId} /> */}
-        </div>
-        // <Container>
-        //     <Row>
-        //         <Col>
-        //             {/* {display==""?<div/>:(display=="response"?<Summary responses={responses} ></Summary>:<Response responses={responses}></Response>)} */}
-        //         </Col>
-        //         <Col>
-        //         </Col>
-        //     </Row>
-        // </Container>
+        // <div>
+        //     {console.log("OK")}
+        // </div>
+        <Container>
+            <Row>
+                <Col md={10} >
+                    <CenteredTabs change={change}></CenteredTabs>
+                    {/* {display==""?<div/>:(display=="response"?<Summary responses={responses} ></Summary>:<Response responses={responses}></Response>)} */}
+                </Col>
+                <Col md={2} style={{ float: "right", marginTop: "5px" }}>
+                    <ExportResponse formId={formId} />
+                </Col>
+            </Row>
+        </Container>
     )
 }
 export default ResponseSummary;
