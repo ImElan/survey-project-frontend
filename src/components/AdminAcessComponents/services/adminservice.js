b60ac50e88f5600ecc16fc6dbcc5f3bd00766f93
hkumar105 <harishkumar0520@gmail.com>
2021-08-29 17:04:55 +0530

    admin access done

--- src/components/AdminAcessComponents/services/adminservice.js
+++ src/components/AdminAcessComponents/services/adminservice.js
@@ -3,25 +3,25 @@ import http from "./http-common";
 
 class AdminDataService{
     doGetById(email){
-        return http.get(`/${email}`);
+        // return http.get(`/${email}`);
 
-        // return http.get(`/api/auth/user/${email}`);
+        return http.get(`/api/auth/user/${email}`);
     }
 
     doUpdate(emaill, rolee){
         // console.log(id);
         // console.log(role);
-        return http.put(`/${emaill}`,{role: rolee});   
+        // return http.put(`/${emaill}`,{role: rolee});   
         // role = {"role":"PM"}
         // {role: "PM","HR"}
 
-        // var objj = {email: emaill, role: rolee}
-        // let tokenn = localStorage.getItem("accessToken");
-        // return http.put(`/api/auth/grantAccess`,objj,{
-        //     headers:{
-        //         Authorization : tokenn 
-        //     }
-        // });
+        var objj = {email: emaill, role: rolee}
+        let tokenn = localStorage.getItem("accessToken");
+        return http.patch(`/api/auth/grantAccess`,objj,{
+            headers:{
+                Authorization : tokenn 
+            }
+        });
     }
 }
 
--- src/components/AdminAcessComponents/services/http-common.js
+++ src/components/AdminAcessComponents/services/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/employees",
-  // baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:3000/employees",
+  baseURL: "http://localhost:8080",
   // Accept: 'application/json',
   // "Access-Control-Origin": "*"
   headers: {
